d0fce2baee6064c41057bb587492b3d9493d201f
HarryJohn <harijaona@digital.gov.mg>
2021-12-21 17:23:03 +0300

    Nouveau projet Flask

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+# pylint: disable=missing-docstring
+
+from flask import Flask
+from flask_restx import Api
+
+def create_app():
+    app = Flask(__name__)
+    app.config['ERROR_404_HELP'] = False
+
+    from .apis.tweets import api as tweets
+    api = Api()
+    api = Api(app, version='1.0', title='Twitter API', description='A simple REST API')
+    # api.init_app(app)
+    api.add_namespace(tweets)
+    
+    return app
+    
