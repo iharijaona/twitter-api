46053adf4fb249beff48cd40654f33a9c177503e
HarryJohn <hravelondrina@gmail.com>
2021-12-22 00:15:33 +0300

    Create tweet

--- app/repositories.py
+++ app/repositories.py
@@ -1,5 +1,8 @@
 # pylint: disable=missing-docstring
 
+from app.models import Tweet
+
+
 class TweetRepository:
     def __init__(self):
         self.clear()
@@ -8,13 +11,14 @@ class TweetRepository:
         self.tweets.append(tweet)
         tweet.id = self.next_id
         self.next_id += 1
+        return tweet
 
     def get(self, id):
-      for tweet in self.tweets:
-          if tweet.id == id:
-              return tweet
-      return None
+        for tweet in self.tweets:
+            if tweet.id == id:
+                return tweet
+        return None
 
     def clear(self):
-      self.tweets = []
-      self.next_id = 1
+        self.tweets = []
+        self.next_id = 1
--- tests/apis/test_tweet_view.py
+++ tests/apis/test_tweet_view.py
@@ -18,7 +18,26 @@ class TestTweetViews(TestCase):
         tweet_repository.add(first_tweet)
         response = self.client.get("/tweets/1")
         response_tweet = response.json
-        print(response_tweet)
         self.assertEqual(response_tweet["id"], 1)
         self.assertEqual(response_tweet["text"], "First tweet")
+        self.assertIsNotNone(response_tweet["created_at"])
+
+    def test_tweets_show(self):
+        first_tweet = Tweet("First tweet")
+        tweet_repository.add(first_tweet)
+        response = self.client.get("/tweets/")
+        response_tweets = response.json
+        self.assertIsInstance(response_tweets, list)
+        self.assertEqual(response_tweets[0]["id"], 1)
+        self.assertEqual(response_tweets[0]["text"], "First tweet")
+        self.assertIsNotNone(response_tweets[0]["created_at"])
+
+    def test_tweet_create(self):
+        response = self.client.post("/tweets/", json={
+            "text": "The created tweet"
+        })
+        response_tweet = response.json
+        self.assertEqual(response.status_code, 201)
+        self.assertIsInstance(response_tweet["id"], int)
+        self.assertEqual(response_tweet["text"], "The created tweet")
         self.assertIsNotNone(response_tweet["created_at"])
